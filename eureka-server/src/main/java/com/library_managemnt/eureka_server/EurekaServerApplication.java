2cd5cdc61db315807fb843732baf1895ed4b5434
asseBadiane <badianeasse99@gmail.com>
2025-07-29 14:01:04 +0000

    ✅ Add eureka-server

---
+++ eureka-server/src/main/java/com/library_managemnt/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.library_managemnt.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Service Discovery Server
+ * Provides service registration and discovery for all microservices
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ eureka-server/src/test/java/com/library_managemnt/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_managemnt.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
